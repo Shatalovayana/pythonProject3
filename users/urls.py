649acc994eaac859a93d56467fa38dafa18998e3
Yana <yanashat198@gmail.com>
2024-02-16 11:40:36 +0700

    add urls and settings

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,11 @@ from rest_framework.routers import DefaultRouter
 from users.apps import UsersConfig
 from users.views import UsersViewSet, PaymentsListAPIView, PaymentsCreateAPIView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 app_name = UsersConfig.name
 
 router = DefaultRouter()
@@ -12,4 +17,6 @@ router.register(r'users', UsersViewSet, basename='users')
 urlpatterns = [
     path('payments/create/', PaymentsCreateAPIView.as_view(), name='payments-create'),
     path('payments/', PaymentsListAPIView.as_view(), name='payments-list'),
-] + router.urls
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    ] + router.urls
